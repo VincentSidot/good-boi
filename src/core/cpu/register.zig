35a0ec17a1f6af17b91665a486cea4437f99ea76
Vincent S <v.sidot@aniah.fr>
2025-11-11 22:27:37 +0100

    Add raw access to registers

--- src/core/cpu/register.zig
+++ src/core/cpu/register.zig
@@ -5,7 +5,7 @@ comptime {
     if (@sizeOf(Flags) != 1) @compileError("Flags must be 1 byte.");
 }
 
-pub const Flags = packed struct {
+pub const FlagsLe = packed struct {
     __unused: u4 = 0,
 
     c: bool = false, // Carry Flag
@@ -13,10 +13,25 @@ pub const Flags = packed struct {
     n: bool = false, // Subtract Flag
     z: bool = false, // Zero Flag
 
-    pub inline fn zeroed() Flags {
-        return Flags{};
+    pub inline fn zeroed() FlagsLe {
+        return FlagsLe{};
     }
 };
+
+pub const FlagsBe = packed struct {
+    z: bool = false, // Zero Flag
+    n: bool = false, // Subtract Flag
+    h: bool = false, // Half Carry Flag
+    c: bool = false, // Carry Flag
+
+    __unused: u4 = 0,
+
+    pub inline fn zeroed() FlagsBe {
+        return FlagsBe{};
+    }
+};
+
+pub const Flags = if (builtin.target.cpu.arch.endian() == .little) FlagsLe else FlagsBe;
 
 const Pairs = packed struct {
     af: u16 = 0,
@@ -26,6 +41,15 @@ const Pairs = packed struct {
 
     sp: u16 = 0,
     pc: u16 = 0,
+};
+
+pub const Register16 = enum(usize) {
+    af = 0,
+    bc = 1,
+    de = 2,
+    hl = 3,
+    sp = 4,
+    pc = 5,
 };
 
 const SingleLE = packed struct {
@@ -44,6 +68,17 @@ const SingleLE = packed struct {
     __unused: u32 = 0, // PC and SP are not part of 8-bit registers
 };
 
+pub const Register8Le = enum(usize) {
+    f = 0,
+    a = 1,
+    c = 2,
+    b = 3,
+    e = 4,
+    d = 5,
+    l = 6,
+    h = 7,
+};
+
 const SingleBE = packed struct {
     a: u8 = 0,
     f: Flags = .{},
@@ -60,8 +95,27 @@ const SingleBE = packed struct {
     __unused: u32 = 0, // PC and SP are not part of 8-bit registers
 };
 
+pub const Register8Be = enum(usize) {
+    a = 0,
+    f = 1,
+    b = 2,
+    c = 3,
+    d = 4,
+    e = 5,
+    h = 6,
+    l = 7,
+};
+
 // Select the correct endianess for the Single representation
 const Single = if (builtin.target.cpu.arch.endian() == .little) SingleLE else SingleBE;
+const Register8 = if (builtin.target.cpu.arch.endian() == .little) Register8Le else Register8Be;
+
+comptime {
+    // Sanity: Ensure Single and Pairs have the same size
+    if (@sizeOf(Single) != @sizeOf(Pairs)) {
+        @compileError("Single and Pairs size mismatch");
+    }
+}
 
 /// Cpu registers
 pub const Registers = packed union {
@@ -74,6 +128,36 @@ pub const Registers = packed union {
 
     pub inline fn zeroed() Registers {
         return Registers{ .pair = .{} };
+    }
+
+    const _rawSingle = *[@bitSizeOf(Registers) / @bitSizeOf(u8)]u8;
+    inline fn asRawSingle(self: *Registers) _rawSingle {
+        return @as(_rawSingle, @ptrCast(self));
+    }
+
+    pub inline fn get8(self: *Registers, reg: Register8) u8 {
+        const raw = self.asRawSingle();
+        return raw[@intFromEnum(reg)];
+    }
+
+    pub inline fn set8(self: *Registers, reg: Register8, value: u8) void {
+        const raw = self.asRawSingle();
+        raw[@intFromEnum(reg)] = value;
+    }
+
+    const _rawPairs = *[@bitSizeOf(Registers) / @bitSizeOf(u16)]u16;
+    inline fn asRawPairs(self: *Registers) _rawPairs {
+        return @as(_rawPairs, @ptrCast(self));
+    }
+
+    pub inline fn get16(self: *Registers, reg: Register16) u16 {
+        const raw = self.asRawPairs();
+        return raw[@intFromEnum(reg)];
+    }
+
+    pub inline fn set16(self: *Registers, reg: Register16, value: u16) void {
+        const raw = self.asRawPairs();
+        raw[@intFromEnum(reg)] = value;
     }
 };
 
@@ -137,4 +221,17 @@ test "regsiter" {
 
     try std.testing.expect(reg2.single.h == 0xBA);
     try std.testing.expect(reg2.single.l == 0xBE);
+}
+
+test "raw access" {
+    const std = @import("std");
+
+    var reg = Registers.zeroed();
+
+    reg.set8(Register8.a, 0x12);
+    reg.set16(Register16.bc, 0xDEAD);
+
+    try std.testing.expect(reg.get16(Register16.af) == 0x1200);
+    try std.testing.expect(reg.get8(Register8.b) == 0xde);
+    try std.testing.expect(reg.get8(Register8.c) == 0xad);
 }
--- src/root.zig
+++ src/root.zig
@@ -2,5 +2,5 @@
 const std = @import("std");
 
 test {
-    std.testing.refAllDecls(@import("register.zig"));
+    std.testing.refAllDecls(@import("./core/cpu/register.zig"));
 }
